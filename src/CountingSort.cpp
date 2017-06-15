34b7dc504015803523b547640623d5df9c1e1425
aclyde11 <aclyde11@gmail.com>
2017-06-15 11:52:39 -0400

    done

--- src/CountingSort.cpp
+++ src/CountingSort.cpp
@@ -8,63 +8,66 @@
 #include "Sort.h"
 #include "CountingSort.h"
 
-template <typename T>
-int CountingSort<T>::max(std::vector<int> vec){
-    int max = INT_MIN;
+template<typename T>
+int CountingSort<T>::max(std::vector<int> vec) {
+  int max = INT_MIN;
 
-    for(auto i : vec)
-        if(i > max)
-            max = i;
+  for (auto i : vec)
+    if (i > max)
+      max = i;
 
-    return max;
+  return max;
 }
 
-template <typename T>
+template<typename T>
 int CountingSort<T>::min(std::vector<int> vec) {
-    int min = INT_MAX;
+  int min = INT_MAX;
 
-    for(auto i : vec)
-        if(i < min)
-            min = i;
+  for (auto i : vec)
+    if (i < min)
+      min = i;
 
-    return min;
+  return min;
 }
 
-
-template <typename T>
+template<typename T>
 std::vector<T> CountingSort<T>::sort() {
-    int M = max(Sort<T>::data), m = min(Sort<T>::data), n = (int) Sort<T>::data.size();
-    int range = M+1;
+  int M = max(Sort<T>::data), m = min(Sort<T>::data), n = (int) Sort<T>::data.size();
+  int range = M + 1;
 
-    //shift sorting vector to [0, n+m]
-    std::vector<int> vec = Sort<T>::data;
-    if (m < 0) {
-        transform(vec.begin(), vec.end(), vec.begin(), bind2nd(std::plus<int>(), -1 * m));
-        range += m;
-    }
+  //shift sorting vector to [0, n+m]
+  std::vector<int> vec = Sort<T>::data;
+  if (m < 0) {
+    std::transform(vec.begin(), vec.end(), vec.begin(), std::bind2nd(std::plus<int>(), -1 * m));
+    range += m;
+  }
 
-    //fill in count vector
-    std::vector<int> counts;
-    counts.assign((size_t) (range), 0);
-    for (auto i : vec) {
-        counts[i]++;
-    }
+  //fill in count vector
+  std::vector<int> counts;
+  counts.assign((size_t) (range), 0);
+  for (auto i : vec) {
+    counts[i]++;
+  }
 
-    //cumulative sum
-    for (int i = 1; i < range; i++) {
-        counts[i] += counts[i-1];
-    }
+  //cumulative sum
+  for (int i = 1; i < range; i++) {
+    counts[i] += counts[i - 1];
+  }
 
-    //look up spot in counts
-    for (int i = 0; i < n; i++)
-        Sort<T>::sorted_data[counts[vec[i]] - 1] = vec[i];
+  //look up spot in counts
+  for (int i = 0; i < n; i++)
+    Sort<T>::sorted_data[counts[vec[i]] - 1] = vec[i];
 
-    //shift vector back
-    if (m < 0) {
-        transform(Sort<T>::sorted_data.begin(), Sort<T>::sorted_data.end(), Sort<T>::sorted_data.begin(), bind2nd(std::plus<int>(), m));
-    }
+  //shift vector back
+  if (m < 0) {
+    std::transform(Sort<T>::sorted_data.begin(),
+                   Sort<T>::sorted_data.end(),
+                   Sort<T>::sorted_data.begin(),
+                   std::bind2nd(std::plus<int>(), m));
+  }
 
-    return Sort<T>::sorted_data;
+  return Sort<T>::sorted_data;
 }
 
-template class CountingSort<int>;
+template
+class CountingSort<int>;
